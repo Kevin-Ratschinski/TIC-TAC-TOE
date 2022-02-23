fd3f6abb191ba814126f4014efda514061d7e652
Kevin Ratschinski <k.ratschinski@icloud.com>
2022-02-23 13:59:52 +0100

    managing game rounds

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   { name: '', symbol: 'X' },
@@ -9,12 +10,14 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const startNewGameBtnElement = document.getElementById('start-game-btn');
-const gameAreaElement = document.getElementById('active-game');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -25,3 +28,5 @@ backdropElement.addEventListener('click', closePlayerConfig);
 formElement.addEventListener('submit', savePlayerConfig);
 
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+gameBoardElement.addEventListener('click', selectGameField);
